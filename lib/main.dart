4bd51212b4c8dcad6e49b150f44888f423016047
zohaib1397 <zohaib.ahmed1397@gmail.com>
2022-09-23 23:16:46 +0500

    Added the true and false tick capability along with the questions

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,21 @@ class _quiz_homeState extends State<quiz_home> {
     );
   }
 
+  List<Widget> recordKeeper = [];
+  List questions = [
+    'My name is Zohaib Ahmed',
+    'Osama is a wonderful person',
+    'Moiz is the nicest person in the world',
+    'I have a Potato friend named Osama',
+  ];
+  List<bool> answers = [
+    true,
+    false,
+    true,
+    true,
+  ];
+  int currentQuestion = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -53,22 +68,68 @@ class _quiz_homeState extends State<quiz_home> {
             children: [
               Expanded(
                 child: Container(
-                  child:
-                      Center(child: customText(message: 'This is the message')),
+                  child: Center(
+                      child: customText(
+                    message: questions[currentQuestion],
+                  )),
                 ),
               ),
               Container(
                 child: Column(
                   children: [
                     customButton(
-                      text: 'Yes',
-                      onPressed: () {},
+                      text: 'True',
+                      onPressed: () {
+                        setState(() {
+                          if (answers[currentQuestion]) {
+                            recordKeeper.add(const Icon(
+                              Icons.check,
+                              color: Colors.green,
+                            ));
+                          } else {
+                            recordKeeper.add(const Icon(
+                              Icons.close,
+                              color: Colors.red,
+                            ));
+                          }
+                          if (currentQuestion == 3) {
+                            currentQuestion = 0;
+                          } else {
+                            currentQuestion++;
+                          }
+                        });
+                      },
                     ),
                     customButton(
-                      text: 'No',
+                      text: 'False',
                       color: Colors.red,
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          if (answers[currentQuestion] == false) {
+                            recordKeeper.add(const Icon(
+                              Icons.check,
+                              color: Colors.green,
+                            ));
+                          } else {
+                            recordKeeper.add(const Icon(
+                              Icons.close,
+                              color: Colors.red,
+                            ));
+                          }
+                          if (currentQuestion == 3) {
+                            currentQuestion = 0;
+                          } else {
+                            currentQuestion++;
+                          }
+                        });
+                      },
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Row(
+                        children: recordKeeper,
+                      ),
+                    )
                   ],
                 ),
               ),
